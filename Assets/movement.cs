af51e6d0ad08bd2cba9370fe41d3feccbec61d65
martinnnnnn <martin.penard@gmail.com>
2018-08-11 13:45:23 +0200

    project creation

---
+++ Assets/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public float speed = 500f;
+    public float jumpforce = 50;
+    
+    
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Vector3 direction = GetComponent<Rigidbody>().velocity;
+
+        direction.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            direction.y = jumpforce;
+        }
+
+        GetComponent<Rigidbody>().velocity = new Vector3(direction.x, direction.y);
+
+
+    }
+}
